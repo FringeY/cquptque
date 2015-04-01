f64867dceee894e52c8f2d426ed3584dd2fe375b
frozneme <465964690@qq.com>
2015-04-02 00:21:18 +0800

    debug

--- App/Runtime/Data/_fields/cqupt_question.que_table_info.php
+++
@@ -1,1 +0,0 @@
-a:5:{i:0;s:11:"que_type_id";i:1;s:4:"name";i:2;s:9:"que_table";s:3:"_pk";s:11:"que_type_id";s:5:"_type";a:3:{s:11:"que_type_id";s:16:"int(11) unsigned";s:4:"name";s:11:"varchar(25)";s:9:"que_table";s:11:"varchar(25)";}}
