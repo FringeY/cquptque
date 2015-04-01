8e1b1ff3aad43f367f844e405f592148f43649bf
frozneme <465964690@qq.com>
2015-04-01 23:55:34 +0800

    debug

--- App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
+++ App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
@@ -384,9 +384,9 @@
     wx.config({
 	    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 	    appId: 'wx81a4a4b77ec98ff4', // 必填，公众号的唯一标识
-	    timestamp: '{Js.timestamp}', // 必填，生成签名的时间戳
-	    nonceStr: '{Js.nonceStr}', // 必填，生成签名的随机串
-	    signature: '{Js.signature}',// 必填，签名，见附录1
+	    timestamp: '<?php echo ($Js["timestamp"]); ?>', // 必填，生成签名的时间戳
+	    nonceStr: '<?php echo ($Js["nonceStr"]); ?>', // 必填，生成签名的随机串
+	    signature: '<?php echo ($Js["signature"]); ?>',// 必填，签名，见附录1
 	    jsApiList: [
 	    	'onMenuShareTimeline',
 	    	'onMenuShareAppMessage',
