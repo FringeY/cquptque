53b8b668344c5b5e673f32d40993427f37ece145
frozneme <465964690@qq.com>
2015-04-02 00:34:02 +0800

    debug

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -13,8 +13,8 @@ return array(
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
 	'DB_HOST'               =>  'localhost', // 服务器地址
 	'DB_NAME'               =>  'cqupt_question',    // 数据库名
-	'DB_USER'               =>  'root',      // 用户名
-	'DB_PWD'                =>  '182617',          // 密码
+	'DB_USER'               =>  'redrock',      // 用户名
+	'DB_PWD'                =>  'cqupt_question',          // 密码
 	'DB_PORT'               =>  '3306',        // 端口
 	'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
 	
